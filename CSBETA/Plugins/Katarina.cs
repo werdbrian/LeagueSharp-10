df49e67b961f4448553afec592bc7c3710cd3f05
spudgy <admin@gamesteroids.com>
2015-06-25 08:43:42 +0300

    qwec

---
+++ CSBETA/Plugins/Katarina.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengerSeries.Plugins
+{
+    internal class Katarina : ChallengerPlugin
+    {
+        public Katarina()
+        {
+            InitChallengerSeries();
+        }
+    }
+}
---
+++ CSBETA/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ChallengerSeries")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bot of Legends")]
+[assembly: AssemblyProduct("ChallengerSeries")]
+[assembly: AssemblyCopyright("Copyright © GoS 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b6e18661-b2b0-457f-bc39-6fe72fa5e950")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1.42")]
+[assembly: AssemblyFileVersion("1.0.1.42")]
