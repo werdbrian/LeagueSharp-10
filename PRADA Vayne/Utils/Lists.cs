8beab77d438094988fb1f303a49d9bca3c81daa1
spudgy <admin@gamesteroids.com>
2015-08-03 22:26:27 +0300

    cqwec

--- PRADA Vayne/Utils/Lists.cs
+++ PRADA Vayne/Utils/Lists.cs
@@ -4,7 +4,7 @@ namespace PRADA_Vayne.Utils
 {
     internal static class Lists
     {
-        internal static List<string> CancerChamps = new List<string> { "Akali", "Talon", "Rengar", "Fizz", "Nocturne", "XinZhao", "Maokai", "Diana", "Vi", "KhaZix", "Riven", "Jayce", "Zac", "Renekton", "Malphite", "Poppy", "Jax", "Tryndamere", "Hecarim", "Quinn", "Irelia", "LeeSin", "Alistar", "Pantheon", "Nidalee" };
+        internal static List<string> CancerChamps = new List<string> { "Akali", "Hecarim", "JarvanIV", "Talon", "Rengar", "Fizz", "Nocturne", "XinZhao", "Maokai", "Diana", "Vi", "KhaZix", "Riven", "Jayce", "Zac", "Renekton", "Sejuani", "Graves", "Malphite", "Poppy", "Jax", "Tryndamere", "Hecarim", "Quinn", "Irelia", "LeeSin", "Alistar", "Pantheon", "Nidalee" };
         internal static List<string> UselessChamps = new List<string> {"Taric"};
     }
 }
